75b7b96349d940d9ac7093c458b0bbb76131a3b5
Thomas Ricketts <tbrricketts@gmail.com>
2025-05-10 13:42:35 -0700

    Set import * behavior in __init__.py files for the model and data fetcher modules.

---
+++ src/monte_carlo_simulator/data_fetcher/__init__.py
@@ -0,0 +1,4 @@
+
+from .market_data_fetcher import MarketDataFetcher, CachedLimiterSession
+
+__all__ = ["MarketDataFetcher", "CachedLimiterSession"]
---
+++ src/monte_carlo_simulator/model/__init__.py
@@ -0,0 +1,7 @@
+
+from .financial_asset import FinancialAsset
+from .market_index import MarketIndex
+from .stock import Stock
+from .risk_free_security import RiskFreeSecurity
+
+__all__ = ["FinancialAsset", "MarketIndex", "Stock", "RiskFreeSecurity"]
